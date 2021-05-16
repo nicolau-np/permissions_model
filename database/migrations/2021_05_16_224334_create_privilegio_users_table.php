43472a4eb89658239e1035e08131c5c93a30d8b7
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-16 23:57:09 +0100

    terminada a migration privilegios

--- database/migrations/2021_05_16_224334_create_privilegio_users_table.php
+++ database/migrations/2021_05_16_224334_create_privilegio_users_table.php
@@ -14,8 +14,18 @@ class CreatePrivilegioUsersTable extends Migration
     public function up()
     {
         Schema::create('privilegio_users', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->bigInteger('id_usuario')->unsigned()->index();
+            $table->bigInteger('id_permissao')->unsigned()->index();
+            $table->bigInteger('id_grupo')->unsigned()->index();
+            $table->string('estado');
             $table->timestamps();
+        });
+
+        Schema::table('privilegio_users', function (Blueprint $table) {
+            $table->foreign('id_usuario')->reference('id')->on('usuarios')->onUpdate('cascade');
+            $table->foreign('id_permissao')->reference('id')->on('permissaos')->onUpdate('cascade');
+            $table->foreign('id_grupo')->reference('id')->on('grupos')->onUpdate('cascade');
         });
     }
 
