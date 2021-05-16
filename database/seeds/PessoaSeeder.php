5ef0060ae9c55d2641340f630357b79bd870de65
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 00:24:10 +0100

    trabalhando na seeder pessoa

--- database/seeds/PessoaSeeder.php
+++ database/seeds/PessoaSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PessoaSeeder extends Seeder
 {
@@ -9,8 +10,23 @@ class PessoaSeeder extends Seeder
      *
      * @return void
      */
+
+    static $pessoas = [
+        [
+            'nome' => "Nicolas Cage",
+            'genero' => "Masculino",
+        ], [
+            'nome' => "Dores Nicodemus",
+            'genero' => "Femenino",
+        ]
+    ];
+
     public function run()
     {
-        //
+        foreach (Self::$pessoas as $pessoa) {
+            DB::table('pessoa')->insert(
+                $pessoa
+            );
+        }
     }
 }
