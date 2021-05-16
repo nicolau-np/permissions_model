621e69a00082807159f899d32dff6ca7d20fce3c
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 00:27:37 +0100

    trabalhando na seeder usuarios

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -9,8 +11,23 @@ class UserSeeder extends Seeder
      *
      * @return void
      */
+
+
     public function run()
     {
-        //
+        DB::table('usuarios')->insert([
+            array(
+                'id_pessoa'=>1,
+                'email'=>"nic340k@gmail.com",
+                'password' =>Hash::make('olasolas'),
+                'estado' =>"on",
+            ),
+            array(
+                'id_pessoa'=>2,
+                'email'=>"armindadorescatotak@gmail.com",
+                'password' =>Hash::make('dorescatota'),
+                'estado' =>"on",
+            ),
+        ]);
     }
 }
