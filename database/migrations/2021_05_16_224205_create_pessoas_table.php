b24b5c8ba3c1a924e88c4fd202063372f46e2806
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-16 23:47:28 +0100

    terminada a migration pessoa

--- database/migrations/2021_05_16_224205_create_pessoas_table.php
+++ database/migrations/2021_05_16_224205_create_pessoas_table.php
@@ -14,7 +14,9 @@ class CreatePessoasTable extends Migration
     public function up()
     {
         Schema::create('pessoas', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('nome')->unique();
+            $table->string('genero');
             $table->timestamps();
         });
     }
