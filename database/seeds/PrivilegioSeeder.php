53d1029819d174fff3c1b02f16a88c01c3164ca9
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 00:37:13 +0100

    trabalhando na seeder privilegioUser

--- database/seeds/PrivilegioSeeder.php
+++ database/seeds/PrivilegioSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class PrivilegioSeeder extends Seeder
 {
@@ -9,8 +10,35 @@ class PrivilegioSeeder extends Seeder
      *
      * @return void
      */
+
+     static $privilegio_users = [
+         [
+           'id_permissao' =>1,
+           'id_grupo'=>3,
+           'id_usuario' =>1,
+           'estado' =>"on"
+         ],[
+            'id_permissao' =>2,
+            'id_grupo'=>3,
+            'id_usuario' =>1,
+            'estado' =>"on"
+          ],
+
+
+          [
+            'id_permissao' =>1,
+            'id_grupo'=>1,
+            'id_usuario' =>2,
+            'estado' =>"on"
+          ],
+     ];
+
     public function run()
     {
-        //
+        foreach(Self::$privilegio_users as $privilegio_user){
+            DB::table('privilegio_users')->insert(
+                $privilegio_user
+            );
+        }
     }
 }
