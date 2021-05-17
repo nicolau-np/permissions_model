cd611b6481fb4ea27475808e8b0de9c001b83123
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 18:49:17 +0100

    trabalhando no ficheiro edit

--- resources/views/administrador/edit.blade.php
+++ resources/views/administrador/edit.blade.php
@@ -1,1 +1,10 @@
-E
+@extends('layout.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                Edicao
+            </div>
+        </div>
+    </div>
+@endsection
