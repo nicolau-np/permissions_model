84ca113afbd0e763dd12346014c724bd6f8e44d6
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 11:24:12 +0100

    linkar arquivo css no app

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -6,6 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Permission Model</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="{{assets('assets/css/style.css')}}">
 </head>
 <body>
 
