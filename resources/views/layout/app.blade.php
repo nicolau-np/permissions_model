baf1ed43113d1f09aaaec1be937f56ef4f5fe548
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 19:03:32 +0100

    alterando app.blade

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Permission Model</title>
+    <title>{{$title}}</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css">
     <link rel="stylesheet" href="{{asset('assets/css/style.css')}}">
 </head>
