b663f479201867a1be41de36758d09ad714e1462
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 11:44:56 +0100

    trabalahndo na tela principal

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -79,14 +79,10 @@
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="/administrador">Admin</a>
+                    <a href="/recursos">R.H</a>
+                    <a href="/secretaria">Secretaria</a>
+                    <a href="/sobre">Sobre</a>
                 </div>
             </div>
         </div>
