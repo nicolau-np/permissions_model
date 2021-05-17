ee63e304c8b58fb346f825eb7f29521a2b8ec9c8
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 12:05:10 +0100

    criando tela de erro 500

---
+++ resources/views/errors/500.blade.php
@@ -0,0 +1,15 @@
+@extends('layout.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12" style="text-align: center;">
+                <div class="card">
+                    <div class="card-body">
+                         <h3>Erro 500</h3>
+                        Problemas no servidor <a href="/admin/home">Principal</a>
+                    </div>
+                </div>
+              </div>
+        </div>
+    </div>
+@endsection
