a9f1964b927314b66809349e5b22508a32e855fd
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 11:57:10 +0100

    criando tela de sobre

---
+++ resources/views/sobre.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.app')
+@section('content')
+<div class="row">
+    <div class="col-md-12">
+        <div class="card">
+            <div class="card-body">
+                <h1>Permission Model</h1>
+                <p>Um modelo de Permissões em Laravel, como deve ser um sistema no que tange a privacidade de cada usuário</p>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
