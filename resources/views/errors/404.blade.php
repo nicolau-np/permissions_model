24380e957f656f8bb4b0899b975c2906cc29a9c6
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 12:03:11 +0100

    criando pagina de erro 404

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,11 @@
+@extends('layout.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h3>Erro 404</h3>
+                Pagina Não encontrada <a href="/admin/home">Principal</a>
+            </div>
+        </div>
+    </div>
+@endsection
