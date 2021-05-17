a091b1ee8cfffc45200cdb154adafd6b5fa31b8e
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 11:19:23 +0100

    adicionando bootstrap no layout

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Permission Model</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha2/css/bootstrap.min.css">
 </head>
 <body>
 
