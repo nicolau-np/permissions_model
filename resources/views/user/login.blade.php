a422a8af362c590d0ff11fb9d2d1fa350dc60403
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 11:14:45 +0100

    tela de login trabalhando 2

--- resources/views/user/login.blade.php
+++ resources/views/user/login.blade.php
@@ -1,5 +1,11 @@
 <div class="form">
     {{Form::open(['method'=>"post", 'action'=>"{{route('logar')}}"])}}
-        
+        <div class="row">
+            <div class="col-md-12">
+                <div class="form-group">
+                    
+                </div>
+            </div>
+        </div>
     {{Form::close()}}
 </div>
