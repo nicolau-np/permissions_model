1997198434df1a9f9aef6e1c49d7a9354bb3050b
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 19:05:12 +0100

    adicionando array dos titulos nos metodos do home

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 class HomeController extends Controller
 {
     public function index(){
-        return view('home');
+        $data = [
+            'title'=>"Permission Model",
+            'menu'=>"home",
+        ];
+        return view('home', $data);
     }
 
     public function sobre(){
