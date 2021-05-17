f509088786c11b1840effe51f1d52143249ce2ac
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 11:59:27 +0100

    mudando o titulo da pagina

--- resources/views/sobre.blade.php
+++ resources/views/sobre.blade.php
@@ -1,14 +1,17 @@
 @extends('layout.app')
 @section('content')
-<div class="row">
+<div class="container">
+    <div class="row">
     <div class="col-md-12">
         <div class="card">
             <div class="card-body">
-                <h1>Permission Model</h1>
+                <h3>Permission Model</h3>
                 <p>Um modelo de Permissões em Laravel, como deve ser um sistema no que tange a privacidade de cada usuário</p>
             </div>
         </div>
     </div>
 </div>
+</div>
+
 
 @endsection
