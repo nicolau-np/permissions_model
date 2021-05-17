8c73b2d04e6d3cec2f682ec5f37df351209138cb
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-17 18:49:42 +0100

    trabalhando no ficheiro list

---
+++ resources/views/administrador/list.blade.php
@@ -0,0 +1,10 @@
+@extends('layout.app')
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                Listagem
+            </div>
+        </div>
+    </div>
+@endsection
