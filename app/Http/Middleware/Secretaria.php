5f857fbb8dcd83fbaaf7795fc90e5e863e71abf2
Nicolau NP <mr1Normaliii@gmail.com>
2021-05-18 12:36:19 +0100

    trabalhando na middleware Secretaria

--- app/Http/Middleware/Secretaria.php
+++ app/Http/Middleware/Secretaria.php
@@ -3,6 +3,7 @@
 namespace App\Http\Middleware;
 
 use Closure;
+use Illuminate\Support\Facades\Auth;
 
 class Secretaria
 {
@@ -15,6 +16,9 @@ class Secretaria
      */
     public function handle($request, Closure $next)
     {
-        return $next($request);
+        if(Auth::check() && Auth::user()->acesso == "Secretaria"){
+            return $next($request);
+        }
+        return redirect('/');
     }
 }
